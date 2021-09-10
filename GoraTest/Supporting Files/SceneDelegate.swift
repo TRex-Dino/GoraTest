412a7e598b540168714c67923501e985d948fd96
Dima <dmitrysinkov6@gmail.com>
2021-09-10 22:17:11 +0300

    create project structure add folders services - mdoel and networking

---
+++ GoraTest/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  GoraTest
+//
+//  Created by Dmitry on 10.09.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
